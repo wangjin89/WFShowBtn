08a888eec0c2a74d6e2dec5d3f7241d50e84c05d
王飞 <wangfei@wangfeidemac-mini.local>
2016-10-13 13:27:29 +0800

    Initial Commit

---
+++ WFShowBtn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WFShowBtn
+//
+//  Created by 王飞 on 16/10/13.
+//  Copyright © 2016年 WF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WFShowBtn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WFShowBtn
+//
+//  Created by 王飞 on 16/10/13.
+//  Copyright © 2016年 WF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
